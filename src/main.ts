711d7376fc3bd3cbd44a26560f08f7800f1bb52a
jordanoskidavid <jordanoski.david16@gmail.com>
2026-05-17 18:43:28 +0200

    CORS error fixed.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
 }
